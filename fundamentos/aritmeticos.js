66931a25bf7f2921365e982be8bedce94e348b1e
bruno magnus justo <bruno.justo@rede.ulbra.br>
2019-12-28 21:17:32 -0300

    Alterações de sábado

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,14 @@
+const [a,b,c,d] = [3,5,1,15]
+
+const soma = a+b+c+d
+const sub = b-a
+const divisao = d/b
+const multiplicacao = b*d
+const modulo = a % 2
+
+console.log(soma)
+console.log(sub)
+console.log(-sub) // Operador unário, convertendo o resultado de positivo para negativo ou de negativo para positivo.
+console.log(divisao)
+console.log(multiplicacao)
+console.log(modulo)
